b226e09ed4172b3a566a849d9e90495e205d5c23
linyan-w <53352772+linyan-w@users.noreply.github.com>
2021-02-22 14:56:10 -0500

    A simply TDC

---
+++ TDC/TDC.srcs/sim_1/new/testbench.vhd
@@ -0,0 +1,108 @@
+-- Testbench created online at:
+--   https://www.doulos.com/knowhow/perl/vhdl-testbench-creation-using-perl/
+-- Copyright Doulos Ltd
+
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+entity tdc_tb is
+end;
+
+architecture bench of tdc_tb is
+
+  component tdc
+    Port (
+       clk    : in std_logic;
+       g_reset: in std_logic;
+       t_reset: in std_logic;
+       pulse  : in std_logic;
+       o_time : out std_logic_vector(11 downto 0);
+       o_width: out std_logic_vector(11 downto 0);
+       o_valid: out std_logic);
+  end component;
+
+  signal clk: std_logic;
+  signal g_reset: std_logic;
+  signal t_reset: std_logic;
+  signal pulse: std_logic;
+  signal o_time: std_logic_vector(11 downto 0);
+  signal o_width: std_logic_vector(11 downto 0);
+  signal o_valid: std_logic;
+
+  constant clock_period: time := 4 ns;
+  signal stop_the_clock: boolean;
+
+begin
+
+  uut: tdc port map ( clk     => clk,
+                      g_reset => g_reset,
+                      t_reset => t_reset,
+                      pulse   => pulse,
+                      o_time  => o_time,
+                      o_width => o_width,
+                      o_valid => o_valid );
+
+  stimulus: process
+  begin
+  
+    -- Put initialisation code here
+    t_reset <= '0';
+    pulse <= '0';
+    g_reset <= '1';
+    wait for 4 ns;
+    g_reset <= '0';
+    wait for 4 ns;
+    t_reset <= '1';
+    wait for 4 ns;
+    t_reset <= '0';
+    wait for 16 ns;
+    
+    pulse <= '1';
+    wait for 20 ns;
+    pulse <= '0';
+    wait for 12 ns;
+    
+    pulse <= '1';
+    wait for 8 ns;
+    pulse <= '0';
+    wait for 12 ns;
+    
+    t_reset <= '1';
+    wait for 4 ns;
+    t_reset <= '0';
+    wait for 7 ns;
+    
+    pulse <= '1';
+    wait for 11 ns;
+    pulse <= '0';
+    wait for 5 ns;
+    
+    g_reset <= '1';
+    wait for 4 ns;
+    g_reset <= '0';
+    wait for 3 ns;
+    
+    pulse <= '1';
+    wait for 9 ns;
+    pulse <= '0';
+    wait for 4 ns;
+    
+
+    -- Put test bench stimulus code here
+
+    stop_the_clock <= true;
+    wait;
+  end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      clk <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
+  
---
+++ TDC/TDC.srcs/sources_1/new/tdc.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2021/02/16 21:55:13
+-- Design Name: 
+-- Module Name: tdc - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tdc is
+  Port (
+     clk    : in std_logic;
+     g_reset: in std_logic;
+     t_reset: in std_logic;
+     pulse  : in std_logic;
+     o_time : out std_logic_vector(11 downto 0);
+     o_width: out std_logic_vector(11 downto 0);
+     o_valid: out std_logic);
+end tdc;
+
+architecture Behavioral of tdc is
+    signal present_time: unsigned(11 downto 0) := (others => '0');
+    signal start_time: unsigned(11 downto 0) := (others => '0');
+    signal outflag: std_logic := '0';
+    
+begin
+
+    time_counter: process(clk, t_reset, g_reset, pulse)    
+    begin    
+    if(rising_edge(clk)) then
+	if (t_reset='1') then
+	   present_time <= (others => '0');
+    elsif (g_reset = '1') then
+       present_time <= (others => '0');
+	   o_time <= (others => '0');
+       o_width <= (others => '0');
+	   o_valid <= '0';
+	else
+	    present_time <= present_time + 1;
+        if (pulse='1' and outflag='0') then
+		  o_time <= std_logic_vector(present_time);
+		  start_time <= present_time;
+		  outflag <= '1';
+		  o_valid <= '0';
+	   elsif (pulse='0' and outflag='1') then
+		o_width <= std_logic_vector(present_time - start_time);
+    	o_valid <= '1';
+	   	outflag <= '0';
+	   end if;	
+	end if;	
+	end if;
+	end process;
+
+end Behavioral;
