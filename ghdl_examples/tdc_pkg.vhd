82b7ea3f95aeba79b660f8911cba57535e8d7042
Eric Hazen <hazen@bu.edu>
2021-04-01 09:28:14 -0400

    very much work in progress, stalled dealing with structify etc

--- ghdl_examples/tdc_pkg.vhd
+++ ghdl_examples/tdc_pkg.vhd
@@ -7,85 +7,83 @@ use ieee.numeric_std.all;
 
 package tdc_pkg is
 
-  constant NUM_TDC_BUFFERS : integer := 4;      -- number of buffers
-  constant TDC_COARSE_WIDTH : integer := 6;     -- coarse time counter width
+  constant NUM_TDC_BUFFERS  : integer := 4;  -- number of buffers
+  constant TDC_COARSE_BITS : integer := 6;  -- coarse time counter width
+  constant TDC_PHASE_BITS : integer := 2;   -- phase measurement bits
 
-  constant TRIGGER_WINDOW : integer := 25;      -- trig window in clk0 cycles
+  constant TRIGGER_WINDOW : integer := 25;  -- trig window in clk0 cycles
 
   -- size of buffer group
-  constant BUFFER_SIZE : integer := (TDC_COARSE_WIDTH + 2 + 1);
+  constant BUFFER_SIZE       : integer := (TDC_COARSE_WIDTH + 2 + 1);
   constant BUFFER_GROUP_SIZE : integer := NUM_TDC_BUFFERS * BUFFER_SIZE;
 
-  -- new buffer with width etc
-  -- total size ~23 bits
-  type tdc_buff_t is record
-    -- counts down from ~100ns from leading edge
-    window_time : unsigned(TDC_COARSE_WIDTH-1 downto 0);
-    -- stores phase of leading edge
-    leading_edge_phase : std_logic_vector(1 downto 0);
-    -- stores coarse time and phase for trailing edge
-    trailing_edge_time : unsigned(TDC_COARSE_WIDTH-1 downto 0);
-    trailing_edge_phase : std_logic_vector(1 downto 0);
-    -- counts down from end of window to timeout
-    time_out : unsigned(TDC_COARSE_WIDTH-1 downto 0);
-    tdc_live : std_logic;               -- in window
-    tdc_busy : std_logic;               -- waiting for trailing edge
-    tdc_valid : std_logic               -- trigger seen in window
-  end record tdc_buff_t;
-  
+  -- TDC buffer type and support functions
+  type tdc_buffer_rt is record
+    window : unsigned(TDC_COARSE_BITS-1 downto 0);
+    leading_edge_phase : std_logic_vector(TDC_PHASE_BITS-1 downto 0);
+    trailing_edge_time : unsigned(TDC_COARSE_BITS-1 downto 0);
+    trailing_edge_phase : std_logic_vector(TDC_PHASE_BITS-1 downto 0);
+    time_out : unsigned( TDC_COARSE_BITS-1 downto 0);
+    live : std_logic;
+    busy : std_logic;
+    valid : std_logic;
+  end record tdc_buffer_rt;
+  -- hand update the constant below!
+  constant TDC_BUFFER_LEN : integer := (TDC_COARSE_BITS*3+TDC_PHASE_BITS*2+3);
+  subtype tdc_buffer_rvt is std_logic_vector(TDC_BUFFER_LEN-1 downto 0);
+  function vectorify(x: tdc_buffer_rt) return tdc_buffer_rvt;
+  function structify(x: tdc_buffer_rvt) return tdc_buffer_rt;
+  function nullify(x: tdc_buffer_rt) return tdc_buffer_rt;
 
-  -- one TDC output buffer
-  type tdc_buffer_t is record
-    tdc_time : unsigned(TDC_COARSE_WIDTH-1 downto 0);
-    tdc_phase : std_logic_vector(1 downto 0);
-    tdc_valid : std_logic;
-  end record tdc_buffer_t;
+  
 
   -- group of buffers
   type tdc_buffer_group_t is array (0 to NUM_TDC_BUFFERS-1) of tdc_buffer_t;
 
   -- conversion functions to/from SLV (I hate VHDL!)
-  function tbt2slv (tbt : tdc_buffer_t) return std_logic_vector;
-  function tbtg2slv ( tbtg : tdc_buffer_group_t) return std_logic_vector;
+  function vectorify( x: tdc_buffer_t) return 
 
-  function slv2tbt (slv : std_logic_vector(BUFFER_SIZE-1 downto 0)) return tdc_buffer_t;
+  function tbt2slv (tbt   : tdc_buffer_t) return std_logic_vector;
+  function tbtg2slv (tbtg : tdc_buffer_group_t) return std_logic_vector;
+
+  function slv2tbt (slv  : std_logic_vector(BUFFER_SIZE-1 downto 0)) return tdc_buffer_t;
   function slv2tbtg (slv : std_logic_vector(BUFFER_GROUP_SIZE-1 downto 0))
     return tdc_buffer_group_t;
 
   -- constant type used to reset a buffer
   constant zero_buffer : tdc_buffer_t := (
-    tdc_time => to_unsigned(0, TDC_COARSE_WIDTH ),
+    tdc_time  => to_unsigned(0, TDC_COARSE_BITS),
     tdc_phase => (others => '0'),
     tdc_valid => '0'
     );
 
   -- constant types used for simulation testing
   constant test_buffer : tdc_buffer_t := (
-    tdc_time => to_unsigned( 57, TDC_COARSE_WIDTH),
+    tdc_time  => to_unsigned(57, TDC_COARSE_BITS),
     tdc_phase => "10",
     tdc_valid => '0'
     );
 
   constant test1 : tdc_buffer_t := (
-    tdc_time => to_unsigned( 1, TDC_COARSE_WIDTH),
+    tdc_time  => to_unsigned(1, TDC_COARSE_BITS),
     tdc_phase => "10",
     tdc_valid => '0'
     );
 
   constant test2 : tdc_buffer_t := (
-    tdc_time => to_unsigned( 2, TDC_COARSE_WIDTH),
+    tdc_time  => to_unsigned(2, TDC_COARSE_BITS),
     tdc_phase => "10",
     tdc_valid => '0'
     );
 
   constant test3 : tdc_buffer_t := (
-    tdc_time => to_unsigned( 3, TDC_COARSE_WIDTH),
+    tdc_time  => to_unsigned(3, TDC_COARSE_BITS),
     tdc_phase => "10",
     tdc_valid => '0'
     );
 
   constant test4 : tdc_buffer_t := (
-    tdc_time => to_unsigned( 4, TDC_COARSE_WIDTH),
+    tdc_time  => to_unsigned(4, TDC_COARSE_BITS),
     tdc_phase => "10",
     tdc_valid => '0'
     );
@@ -101,19 +99,39 @@ use work.tdc_pkg.all;
 package body tdc_pkg is
 
   -- convert one TDC buffer type to SLV
+  function vectorify(x: tdc_buffer_rt) return tdc_buffer_rvt is
+    variable s : tdc_buffer_rvt;
+  begin
+    s := std_logic_vector(x.window) & x.leading_edge_phse &
+         std_logic_vector(x.trailing_edge_time) & x.trailing_edge_phase &
+         std_logic_vector(x.time_out) & x.live & x.busy & x.valid;
+    return s;
+  end function vectorify;
+
+  -- convert one TDC buffer type to record
+  function structify(x: tdc_buffer_rvt) return tdc_buffer_rt is
+    variable r : tdc_buffer_rt;
+  begin
+    r.window 
+  end function structify;
+  
+  function nullify(x: tdc_buffer_rt) return tdc_buffer_rt;
+
+
+
   function tbt2slv (tbt : tdc_buffer_t) return std_logic_vector is
-    variable slv : std_logic_vector( TDC_COARSE_WIDTH+2+1 -1 downto 0);
+    variable slv : std_logic_vector(TDC_COARSE_BITS+2+1 -1 downto 0);
   begin
     slv := std_logic_vector(tbt.tdc_time) & tbt.tdc_phase & tbt.tdc_valid;
     return slv;
   end;
 
   -- convert a group (array) of TDC buffers to SLV
-  function tbtg2slv ( tbtg : tdc_buffer_group_t) return std_logic_vector is
-    variable slv : std_logic_vector( BUFFER_GROUP_SIZE-1 downto 0);
+  function tbtg2slv (tbtg : tdc_buffer_group_t) return std_logic_vector is
+    variable slv : std_logic_vector(BUFFER_GROUP_SIZE-1 downto 0);
   begin
     for i in tdc_buffer_group_t'range loop
-      slv( ((i+1)*BUFFER_SIZE-1) downto i*BUFFER_SIZE) := tbt2slv(tbtg(i));
+      slv(((i+1)*BUFFER_SIZE-1) downto i*BUFFER_SIZE) := tbt2slv(tbtg(i));
     end loop;
     return slv;
   end;
@@ -122,21 +140,21 @@ package body tdc_pkg is
   function slv2tbt (slv : std_logic_vector(BUFFER_SIZE-1 downto 0)) return tdc_buffer_t is
     variable tbt : tdc_buffer_t;
   begin
-    tbt.tdc_time := unsigned(slv(BUFFER_SIZE-1 downto BUFFER_SIZE-TDC_COARSE_WIDTH));
+    tbt.tdc_time  := unsigned(slv(BUFFER_SIZE-1 downto BUFFER_SIZE-TDC_COARSE_BITS));
     tbt.tdc_phase := slv(2 downto 1);
     tbt.tdc_valid := slv(0);
     return tbt;
   end;
-  
+
   -- convert an SLV to a group of TDC buffers
   function slv2tbtg (slv : std_logic_vector(BUFFER_GROUP_SIZE-1 downto 0)) return tdc_buffer_group_t is
     variable tbtg : tdc_buffer_group_t;
   begin
     for i in tdc_buffer_group_t'range loop
-      tbtg(i) := slv2tbt( slv( ((i+1)*BUFFER_SIZE-1) downto i*BUFFER_SIZE));
+      tbtg(i) := slv2tbt(slv(((i+1)*BUFFER_SIZE-1) downto i*BUFFER_SIZE));
     end loop;
     return tbtg;
   end;
-  
+
 end package body tdc_pkg;
 
