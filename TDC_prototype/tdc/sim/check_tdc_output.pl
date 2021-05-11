ef9fc5bc6bfab9de2bee6f6907507a506b47bb3b
Eric Hazen <hazen@bu.edu>
2021-05-11 16:40:37 -0400

    bleah

--- TDC_prototype/tdc/sim/check_tdc_output.pl
+++ TDC_prototype/tdc/sim/check_tdc_output.pl
@@ -5,6 +5,8 @@
 use strict;
 use Data::Dumper;
 
+my $cdt = 35;			# countdown time constant
+
 # fancy way to return list of unique values from list from perlmaven.com
 sub uniq {
     my %seen;
@@ -23,13 +25,16 @@ while( my $line = <>) {
     my $thing = { tyme => $d[1]};
     if( $d[0] eq 'T') {
 	$thing->{evn} = $d[4];
+	$thing->{ttime} = $d[2];
 	push @trigs, $thing;
-#	print "Save trigger: ", Dumper($thing);
     } elsif( $d[0] eq 'S') {
 	$thing->{evn} = $d[7];
 	$thing->{chan} = $d[2];
+	$thing->{let} = $d[3];	# leading edge time
+	$thing->{lep} = $d[4];	# leading edge phase
+	$thing->{tet} = $d[5];	# trailing edge time
+	$thing->{tep} = $d[6];	# trailing edge phase
 	push @hits, $thing;
-#	print "Save hit: ", Dumper($thing);
     } else {
 	print "WHAT? $line\n";
     }
@@ -42,17 +47,16 @@ print "$ntrig triggers, $nhits hits\n";
 
 # for each trigger, find matching hits
 foreach my $trig ( @trigs ) {
+    my $ttime = $trig->{ttime};
     my $tyme = $trig->{tyme};
     my $evn = $trig->{evn};
-    print "T $tyme $evn: ";
+    print "T $evn $ttime:\n";
     foreach my $hit ( @hits ) {
 	if( $hit->{evn} eq $evn) {
-	    my $hevn = $hit->{evn};
-	    my $htim = $hit->{tyme};
 	    my $chan = $hit->{chan};
-	    my $diff = $tyme - $htim;
-	    print " $diff";
-#	    print "  $hevn $chan $htim $diff\n";
+	    my $ttime = $ttime - $hit->{let};
+	    print " ($chan $ttime)\n";
+
 	}
     }
     print "\n";
