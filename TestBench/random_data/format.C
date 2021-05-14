09e4b183ad9612a22f55fcddeb8ac4573409c16a
Linyan WAN <linyanwan2009@gmail.com>
2021-05-13 21:23:41 -0400

    Update software model

--- TestBench/random_data/format.C
+++ TestBench/random_data/format.C
@@ -1,5 +1,6 @@
-const int ctd=35, window=25, unit=4;
-const int shift_ts=4, shift_tsf=2, shift_te=1, shift_tef=2;
+const int ctd=35, window=25, tolerance=10, unit=4;
+const int nchan=4;
+const int shift_ts=-2, shift_tsf=2, shift_ss=4, shift_ssf=2, shift_se=1, shift_sef=2;
 const int trig_shift=40, pulse_shift=216;
 const double circ_time=1e-5;
 //Now trigger window is [T-(window+shift_ts)*unit, T-shift_ts*unit]=[T-118, T-18].
@@ -13,12 +14,19 @@ void CarryBorrow(int *x, int *y, int dx, int dy){
 	y[0]=y[0]+dy;
 	if(y[0]>=unit){
 		y[0]-=unit;
+		x[0]++;
+	}
+}
+void AddMinus(int *x, int *y, int dx, int dy){
+	x[0]=x[0]+dx;
+	y[0]=y[0]+dy;
+	if(y[0]>=unit){
+		y[0]-=unit;
 	}
 }
 
 void format(){
 	ifstream fi("testbench.dat");
-	const int nchan=4;
 
 	vector<double> stime, etime, ttime, stime_t, etime_t;
 	vector<int> chan, chan_t;
@@ -44,8 +52,9 @@ void format(){
 			etime_t.clear();
 			chan_t.clear();
 			for(int i=stime.size()-1;i>=0;i--){
-				if(stime[i]>ttime.back()-window*unit-shift_ts*unit-shift_tsf){
-					if(stime[i]>ttime.back()-shift_ts*unit-shift_tsf)continue;
+				if(stime[i]>ttime.back()-ctd*unit){
+					if(stime[i]>ttime.back())continue;
+					//if(stime[i]>ttime.back()-shift_ts*unit-shift_tsf)continue;
 					stime_t.push_back(stime[i]-circ_time);
 					etime_t.push_back(etime[i]);
 					chan_t.push_back(chan[i]);
@@ -53,24 +62,27 @@ void format(){
 				else break;
 			}
 
+			t_now=int(ttime.back()/unit)*unit+trig_shift;
+			t_s=int(ttime.back()/unit);
+			t_sf=phase(ttime.back());
+			CarryBorrow(&t_s, &t_sf, shift_ts, shift_tsf);
+			of<<Form("T %i %i %i %i", t_now, t_s, t_sf, trign)<<endl;
+
 			if(stime_t.size()!=0){
-				t_now=int(ttime.back()/unit)*unit+trig_shift;
-				t_s=int(ttime.back()/unit);
-				t_sf=phase(ttime.back());
-				of<<Form("T %i %i %i %i", t_now, t_s, t_sf, trign)<<endl;
 				for(int j=stime_t.size()-1;j>=0;j--){
 					s_now=int(stime_t[j]/unit)*unit+pulse_shift;
 					s_s=ctd+int(stime_t[j]/unit+0.5)-int(ttime.back()/unit);
 					s_sf=phase(stime_t[j]);
 					s_e=ctd+int(stime_t[j]/unit+0.5)-int((stime_t[j]+etime_t[j])/unit+0.5);
 					s_ef=phase(stime_t[j]+etime_t[j]);
-					CarryBorrow(&s_s, &s_sf, shift_ts, shift_tsf);
-					CarryBorrow(&s_e, &s_ef, shift_te, shift_tef);
+					AddMinus(&s_s, &s_sf, shift_ss, shift_ssf);
+					AddMinus(&s_e, &s_ef, shift_se, shift_sef);
 					if(s_s>=35)continue;
+					if(s_s<10)continue;
 					of<<Form("S %i %i %i %i %i %i %-2i", s_now, chan_t[j], s_s, s_sf, s_e, s_ef, trign)<<endl;
 				}
-				trign++;
 			}
 		}
+		trign++;
 	}
 }
